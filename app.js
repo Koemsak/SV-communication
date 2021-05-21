5c6dd0c53b375ae9a144a6a087ac4caa1d637ad2
Koemsak <koemsak.mean@student.passerellesnumeriques.org>
2021-05-21 07:55:41 +0700

    login

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Hello word");
+
+const express = require("express");
+const app = express();
+
+const fs = require("fs");
+
+const PORT = 3000;
+app.listen(PORT, (req, res) => console.log("Server is running...."));
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+// READ FILE
+let data = JSON.parse(fs.readFileSync("data.json"));
+
+
+app.post("/login", (req, res) => {
+    let userLogin = req.body;
+    let isValid = false;
+    for (let user of data) {
+        console.log(user)
+        if (user.name == userLogin.name && user.password == userLogin.password) {
+            isValid = true;
+        }
+        // console.log(user.password);
+    }
+    res.send(isValid);
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
+// REQUEST DATA FROM SERVER
+const IP = "192.168.88.21";
+const PORT =  "3000";
+
+const URL_REQUES = "http://" + IP + ":" + PORT;
+
+// USER ACTION
 let elementShow = (element, show) => {
     if (show) {
         element.style.display = "block";
@@ -16,6 +23,9 @@ function formSignup() {
     elementShow(loginForm, false);
     elementShow(createBtn, true);
     elementShow(singupBtn, false);
+
+    userNameLogin.value = "";
+    passwordLogin.value = "";
 }
 
 let singupBtn = document.querySelector("#btnSignup");
@@ -28,8 +38,43 @@ function formLogin () {
     elementShow(signUpForm, false);
     elementShow(createBtn, false);
     elementShow(singupBtn, true);
+    let obj = {
+        name: userNameLogin.value,
+        password: passwordLogin.value
+    }
+    axios.post(URL_REQUES + "/login", obj).then((response) => {
+        let isValid = response.data;
+        if (isValid) {
+            elementShow(messege, true);
+            elementShow(formContainer, false);
+            elementShow(noInfo, false);
+        } else {
+            elementShow(noInfo, true);
+        }
+    })
+
 }
 
 let loginBtn = document.querySelector("#btnLogin");
 loginBtn.addEventListener('click', formLogin)
+// ALL CHAT
+let messege = document.querySelector(".body-content")
 
+// INFORMATION FROM INDEX 
+let noInfo = document.querySelector("#notCorrectInfo");
+
+// LOGOUT 
+function logout () {
+    elementShow(messege, false);
+    elementShow(formContainer, true);
+    userNameLogin.value = "";
+    passwordLogin.value = "";
+}
+let logoutBtn = document.querySelector("#logout");
+logoutBtn.addEventListener('click', logout);
+
+
+// INFORMATION FROM INPUT
+let userNameLogin = document.querySelector("#username");
+let passwordLogin = document.querySelector("#password");
+let formContainer = document.querySelector(".container");
