b1f75c07a45a27bb95ac96fafe85675a5745b389
Koemsak <koemsak.mean@student.passerellesnumeriques.org>
2021-05-24 17:49:27 +0700

    test

--- public/js/message.js
+++ public/js/message.js
@@ -10,44 +10,49 @@ let user_login = localStorage.getItem("username");
 // MESSAGE CHAT
 
 
-function showMessage() {
-    username.textContent = user_login.toUpperCase();
-    axios.get("/getdata").then((response) => {
-        let container = document.createElement("div");
-        container.className = "container";
-        for (let data of response.data) {
-            let outgoing = document.createElement("div");
-            outgoing.className = "chat outgoing";
-            let incoming = document.createElement("div");
-            incoming.className = "chat incoming";
-            let details = document.createElement("div");
-            details.className = "details";
-            let p = document.createElement("p");
-            let h5 = document.createElement("h5");
-            if (data.name === user_login) {
-                p.textContent = data.text;
-                h5.textContent = data.name.toUpperCase();
-                details.appendChild(p);
-                outgoing.appendChild(details);
-                outgoing.appendChild(h5);
-                container.appendChild(outgoing);
-            } else {
-                p.textContent = data.text;
-                h5.textContent = data.name.toUpperCase();
-                details.appendChild(p);
-                incoming.appendChild(h5);
-                incoming.appendChild(details);
-                container.appendChild(incoming);
-            }
+function showMessage(response) {
+    let chats = document.querySelectorAll(".chat");
+    for (let chat of chats) {
+        if (chat !== null) {
+            chat.remove();
         }
-        chat_box.appendChild(container);
-    })
+    }
+    username.textContent = user_login.toUpperCase();
+    let container = document.createElement("div");
+    container.className = "container";
+    for (let data of response.data) {
+        let outgoing = document.createElement("div");
+        outgoing.className = "chat outgoing";
+        let incoming = document.createElement("div");
+        incoming.className = "chat incoming";
+        let details = document.createElement("div");
+        details.className = "details";
+        let p = document.createElement("p");
+        let h5 = document.createElement("h5");
+        if (data.name === user_login) {
+            p.textContent = data.text;
+            h5.textContent = data.name.toUpperCase();
+            details.appendChild(p);
+            outgoing.appendChild(details);
+            outgoing.appendChild(h5);
+            container.appendChild(outgoing);
+        } else {
+            p.textContent = data.text;
+            h5.textContent = data.name.toUpperCase();
+            details.appendChild(p);
+            incoming.appendChild(h5);
+            incoming.appendChild(details);
+            container.appendChild(incoming);
+        }
+    }
+    chat_box.appendChild(container);
 }
 
 function loadMessage() {
-    let container = chat_box.firstElementChild;
-    container.remove();
-    showMessage();
+    // let container = chat_box.firstElementChild;
+    // container.remove();
+    // showMessage();
+    axios.get("/getdata").then(showMessage);
 }
 
 function send_data() {
@@ -57,7 +62,6 @@ function send_data() {
             text: input_message.value
         }
         axios.post(URL_REQUEST + "/add", allInfo);
-        loadMessage();
         input_message.value = "";
     }
 }
